b667078d42e8f86ec5591fdc73fbee55f456e342
Mario Henrique <mariohenriquelopes3@gmail.com>
2019-10-07 17:44:10 -0300

    bugatti vehicle with threejs and ammojs

---
+++ examples/Carro.js
@@ -0,0 +1,505 @@
+// NECESSARIO IMPORTAR o ammo.js externamente
+import * as THREE from '../build/three.module.js';
+import { FBXLoader } from './jsm/loaders/FBXLoader.js';
+
+function Carro(fisica, sceneP, cameraP, nomeP) {
+	var instancia = this;
+	this.fisica = fisica;
+	var nome = nomeP;
+
+	var scene = sceneP;
+	var camera = cameraP;
+
+	var renderCarro;
+
+	var carregouModelo = false;
+
+	// variaveis camera
+	var firstPerson = false;
+	var flagSomarEsquerda = false;
+	var flagSomarDireita = false;
+	var valorSomarEsquerda = 0;
+	var valorSomarDireita = 0;
+	var velocGiroCamera = 2;
+	var deslocGiroCamera = 2.5;
+	var deslocPraTrasCamera = 4;
+	var margem_up = {
+		margem_1pessoa_up: 0.28,
+		margem_3pessoa_up: 2
+	};
+
+	var duration=150;
+	var snail1 = undefined;
+	var snail2 = undefined;
+
+	// variaveis carro
+	var vehicle;
+	var chassisRigidBody;
+	var chassisMesh;
+	var materialPneu;
+	var materialRoda;	
+	var FRONT_LEFT = 0;
+	var FRONT_RIGHT = 1;
+	var BACK_LEFT = 2;
+	var BACK_RIGHT = 3;
+	var wheelMeshes = [];
+
+	// Var FBX
+	var corpo;
+	var trasesq;
+ 	var trasdir;
+ 	var frenteesq;
+ 	var frentedir;
+
+ 	// Objects Fake Rodas
+ 	var object3DFrenteEsq;
+ 	var object3DFrenteDir;
+
+ 	var object3DTrasEsq;
+ 	var object3DTrasDir;
+
+ 	// - Global variables -
+	var DISABLE_DEACTIVATION = 4;
+	var TRANSFORM_AUX = new Ammo.btTransform();
+	var ZERO_QUATERNION = new THREE.Quaternion(0, 0, 0, 1);
+	var materialInteractive=new THREE.MeshPhongMaterial( { color:0x990000 } );
+
+	// Keybord actions
+	var actions = {};
+	var keysActions = {
+		"KeyW":'acceleration',
+		"ArrowUp":'acceleration',
+		"KeyS":'braking',
+		"ArrowDown":'braking',
+		"KeyA":'left',
+		"ArrowLeft":'left',
+		"KeyD":'right',
+		"ArrowRight":'right'
+	};
+
+	var keyup = function(e) {
+		if(keysActions[e.code]) {
+			actions[keysActions[e.code]] = false;
+			e.preventDefault();
+			e.stopPropagation();
+			return false;
+		}
+		if (e.code == 'KeyX') {
+			firstPerson = !firstPerson;
+		} else if (e.code == 'KeyZ') {
+			var tm = vehicle.getChassisWorldTransform();
+			chassisRigidBody.setLinearVelocity( new Ammo.btVector3( 0, 0, 0 ) );
+			tm.setOrigin(new Ammo.btVector3(-0.00010793397814268246, 1.550489604473114, -19.913904190063477));
+			tm.setRotation(new Ammo.btQuaternion(-0.0026274363044649363, -0.002687657019123435, -0.000005663222964358283, 0.9999929070472717));
+		}
+	};
+	var keydown = function(e) {
+		if(keysActions[e.code]) {
+			actions[keysActions[e.code]] = true;
+			e.preventDefault();
+			e.stopPropagation();
+			return false;
+		}
+	}
+
+	var createWheelMesh = function(radius, width) {
+		if (!materialPneu) {
+			materialPneu = new THREE.MeshBasicMaterial({color: 0x000000});
+		}
+		if (!materialRoda) {
+			materialRoda = new THREE.MeshBasicMaterial({color: 0x9c9898});
+		}
+		var t = new THREE.CylinderGeometry(radius, radius, width, 24, 1);
+		t.rotateZ(Math.PI / 2);
+		var mesh = new THREE.Mesh(t, materialPneu);
+		mesh.add(new THREE.Mesh(new THREE.BoxGeometry(width * 1.5, radius * 1.75, radius*.25, 1, 1, 1), materialRoda));
+		scene.add(mesh);
+		return mesh;
+	};
+
+	var createChassisMesh = function(w, l, h) {
+		var shape = new THREE.BoxGeometry(w, l, h, 1, 1, 1);
+		var mesh = new THREE.Mesh(shape, materialInteractive);
+		scene.add(mesh);
+		return mesh;
+	};
+
+	var createVehicle = function(pos, quat) {
+
+		// Vehicle contants
+
+		var chassisWidth = 1.8;
+		var chassisHeight = .6; // .6
+		var chassisLength = 4;
+		var massVehicle = 800;
+
+		var wheelAxisPositionBack = -1;
+		var wheelHalfTrackBack = 1;
+		var wheelAxisHeightBack = .3;
+		var wheelRadiusBack = .4;
+		var wheelWidthBack = .3;
+
+		var wheelAxisFrontPosition = 1.7;
+		var wheelHalfTrackFront = 1;
+		var wheelAxisHeightFront = .3;
+		var wheelRadiusFront = .35;
+		var wheelWidthFront = .2;
+
+		var friction = 1000;
+		var suspensionStiffness = 20.0; // KIKE
+		var suspensionDamping = 2.3; // Rapidez da maciez na queda
+		var suspensionCompression = 4.4; // trepidação
+		var suspensionRestLength = 0.6;
+		var rollInfluence = 0;
+
+		var steeringIncrement = .04;
+		var steeringClamp = .5;
+		var maxEngineForce = 2000;
+		var maxBreakingForce = 100;
+
+		// Chassis
+		var geometry = new Ammo.btBoxShape(new Ammo.btVector3(chassisWidth * .5, chassisHeight * .5, chassisLength * .5));
+		var transform = new Ammo.btTransform();
+		transform.setIdentity();
+		transform.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
+		transform.setRotation(new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w));
+		var motionState = new Ammo.btDefaultMotionState(transform);
+		var localInertia = new Ammo.btVector3(0, 0, 0);
+		geometry.calculateLocalInertia(massVehicle, localInertia);
+		var body = new Ammo.btRigidBody(new Ammo.btRigidBodyConstructionInfo(massVehicle, motionState, geometry, localInertia));
+		body.setActivationState(DISABLE_DEACTIVATION);
+		body.nome = nome;
+		instancia.fisica.physicsWorld.addRigidBody(body);
+		chassisRigidBody = body;
+		chassisMesh = createChassisMesh(chassisWidth, chassisHeight, chassisLength);
+
+		// Raycast Vehicle
+		var engineForce = 0;
+		var vehicleSteering = 0;
+		var breakingForce = 0;
+		var tuning = new Ammo.btVehicleTuning();
+		var rayCaster = new Ammo.btDefaultVehicleRaycaster(instancia.fisica.physicsWorld);
+		vehicle = new Ammo.btRaycastVehicle(tuning, body, rayCaster);
+		vehicle.setCoordinateSystem(0, 1, 2);
+		instancia.fisica.physicsWorld.addAction(vehicle);
+
+		// Wheels
+		
+		var wheelDirectionCS0 = new Ammo.btVector3(0, -1, 0);
+		var wheelAxleCS = new Ammo.btVector3(-1, 0, 0);
+
+		var addWheel = function(isFront, pos, radius, width, index) {
+
+			var wheelInfo = vehicle.addWheel(
+					pos,
+					wheelDirectionCS0,
+					wheelAxleCS,
+					suspensionRestLength,
+					radius,
+					tuning,
+					isFront);
+
+			wheelInfo.set_m_suspensionStiffness(suspensionStiffness);
+			wheelInfo.set_m_wheelsDampingRelaxation(suspensionDamping);
+			wheelInfo.set_m_wheelsDampingCompression(suspensionCompression);
+			wheelInfo.set_m_frictionSlip(friction);
+			wheelInfo.set_m_rollInfluence(rollInfluence);
+
+			wheelMeshes[index] = createWheelMesh(radius, width);
+		};
+
+		addWheel(true, new Ammo.btVector3(wheelHalfTrackFront, wheelAxisHeightFront, wheelAxisFrontPosition), wheelRadiusFront, wheelWidthFront, FRONT_LEFT);
+		addWheel(true, new Ammo.btVector3(-wheelHalfTrackFront, wheelAxisHeightFront, wheelAxisFrontPosition), wheelRadiusFront, wheelWidthFront, FRONT_RIGHT);
+		addWheel(false, new Ammo.btVector3(-wheelHalfTrackBack, wheelAxisHeightBack, wheelAxisPositionBack), wheelRadiusBack, wheelWidthBack, BACK_LEFT);
+		addWheel(false, new Ammo.btVector3(wheelHalfTrackBack, wheelAxisHeightBack, wheelAxisPositionBack), wheelRadiusBack, wheelWidthBack, BACK_RIGHT);
+
+		// Sync keybord actions and physics and graphics
+		renderCarro = function(dt) {
+
+			var speed = vehicle.getCurrentSpeedKmHour();
+
+			velocGiroCamera = Math.abs(speed) * .66;
+
+			if (velocGiroCamera > 4.2) {
+				velocGiroCamera = 4.2;
+			}
+
+			// velocGiroCamera = 3.2;
+
+			deslocGiroCamera = 7.5 - ( Math.abs(speed) * 4.8 / 50 );
+			if (deslocGiroCamera < 0) {
+				deslocGiroCamera = 0;
+			}
+
+			// speedometer.innerHTML = (speed < 0 ? '(R) ' : '') + Math.abs(speed).toFixed(1) + ' km/h';
+
+			breakingForce = 0;
+			engineForce = 0;
+
+			flagSomarEsquerda = actions.left;
+			flagSomarDireita = actions.right;
+
+			if (actions.acceleration) {
+				if (speed < -1)
+					breakingForce = maxBreakingForce;
+				else engineForce = maxEngineForce;
+			}
+			if (actions.braking) {
+				if (speed > 1)
+					breakingForce = maxBreakingForce;
+				else engineForce = -maxEngineForce / 2;
+			}
+			if (actions.left) {
+				if (vehicleSteering < steeringClamp)
+					vehicleSteering += steeringIncrement;
+			}
+			else {
+				if (actions.right) {
+					if (vehicleSteering > -steeringClamp)
+						vehicleSteering -= steeringIncrement;
+				}
+				else {
+					if (vehicleSteering < -steeringIncrement)
+						vehicleSteering += steeringIncrement;
+					else {
+						if (vehicleSteering > steeringIncrement)
+							vehicleSteering -= steeringIncrement;
+						else {
+							vehicleSteering = 0;
+						}
+					}
+				}
+			}
+
+			vehicle.applyEngineForce(engineForce, BACK_LEFT);
+			vehicle.applyEngineForce(engineForce, BACK_RIGHT);
+
+			vehicle.setBrake(breakingForce / 2, FRONT_LEFT);
+			vehicle.setBrake(breakingForce / 2, FRONT_RIGHT);
+			vehicle.setBrake(breakingForce, BACK_LEFT);
+			vehicle.setBrake(breakingForce, BACK_RIGHT);
+
+			vehicle.setSteeringValue(vehicleSteering, FRONT_LEFT);
+			vehicle.setSteeringValue(vehicleSteering, FRONT_RIGHT);
+
+			var tm, p, q, i;
+			var n = vehicle.getNumWheels();
+			for (i = 0; i < n; i++) {
+				vehicle.updateWheelTransform(i, true);
+				tm = vehicle.getWheelTransformWS(i);
+				p = tm.getOrigin();
+				q = tm.getRotation();
+				wheelMeshes[i].position.set(p.x(), p.y(), p.z());
+				wheelMeshes[i].quaternion.set(q.x(), q.y(), q.z(), q.w());
+			}
+
+			tm = vehicle.getChassisWorldTransform();
+			p = tm.getOrigin();
+			q = tm.getRotation();
+			chassisMesh.position.set(p.x(), p.y(), p.z());
+			chassisMesh.quaternion.set(q.x(), q.y(), q.z(), q.w());
+		}
+	};
+
+	createVehicle(new THREE.Vector3(0, 4, -20), ZERO_QUATERNION);
+
+	var loader = new FBXLoader();
+		 loader.load('bugatti.fbx', function ( object ) {
+		 	
+		 	corpo = object.getObjectByName('corpo');
+		 	corpo.geometry.translate(0, -.4, .36);
+		 	
+		 	trasesq = object.getObjectByName('trasesq');
+		 	trasdir = object.getObjectByName('trasdir');
+		 	frenteesq = trasdir.clone();
+		 	frentedir = trasesq.clone();
+
+		 	object.add(frenteesq);
+		 	object.add(frentedir);
+		 	
+		 	scene.add(object);
+
+		 	object3DFrenteEsq = new THREE.Object3D();
+		 	object3DFrenteDir = new THREE.Object3D();
+		 	object3DTrasEsq = new THREE.Object3D();
+		 	object3DTrasDir = new THREE.Object3D();
+
+		 	carregouModelo = true;
+	 }, function(xhr) {
+	 	document.querySelector('.divLoading').innerHTML = ( xhr.loaded / xhr.total * 100 ).toFixed(2) + '% Loading bugatti';
+	 	if (xhr.loaded == xhr.total) {
+	 		document.querySelector('.divLoading').style.display = 'none';
+	 	}
+	 });
+
+	 this.render = function (dt) {
+
+	 	if (renderCarro) {
+	 		renderCarro(dt);
+	 	}
+
+	 	/**************/
+		// CAMERA
+		// Calcula deslocamento de camera
+		if (flagSomarEsquerda) { valorSomarEsquerda += (dt * velocGiroCamera); } else { valorSomarEsquerda -= (dt * velocGiroCamera); }
+		if (flagSomarDireita) { valorSomarDireita += (dt * velocGiroCamera); } else { valorSomarDireita -= (dt * velocGiroCamera); }				
+		if (valorSomarEsquerda < 0) { valorSomarEsquerda = 0; } else if (valorSomarEsquerda > deslocGiroCamera) { valorSomarEsquerda = deslocGiroCamera; }
+		if (valorSomarDireita < 0) { valorSomarDireita = 0; } else if (valorSomarDireita > deslocGiroCamera) { valorSomarDireita = deslocGiroCamera; }
+
+		if (valorSomarEsquerda > valorSomarDireita) {
+			valorSomarDireita = 0;
+		} else {
+			valorSomarEsquerda = 0;
+		}
+
+
+
+		chassisMesh.visible = false;
+		wheelMeshes[FRONT_LEFT].visible = false;
+		wheelMeshes[FRONT_RIGHT].visible = false;
+		wheelMeshes[BACK_LEFT].visible = false;
+		wheelMeshes[BACK_RIGHT].visible = false;
+
+		if (firstPerson) {
+			if (camera) {
+				camera.position.copy(chassisMesh.position);
+				camera.quaternion.copy(chassisMesh.quaternion);
+				camera.rotateY(Math.PI);
+				camera.translateY(margem_up.margem_1pessoa_up);
+			}
+
+			if (carregouModelo) {
+				corpo.visible = false;
+			 	trasesq.visible = false;
+			 	trasdir.visible = false;
+			 	frenteesq.visible = false;
+			 	frentedir.visible = false;
+			}
+		} else {
+			if (camera) {
+				//camera.position.copy(chassisMesh.position);
+				//camera.quaternion.copy(chassisMesh.quaternion);
+				//camera.rotateY(Math.PI);
+
+				//camera.position.y += margem_up.margem_3pessoa_up;
+				//camera.translateZ(deslocPraTrasCamera);
+				//camera.translateX(-valorSomarEsquerda);
+				//camera.translateX(valorSomarDireita);
+
+				//camera.lookAt(chassisMesh.position);
+				//var d = Math.abs(camera.position.distanceTo( chassisMesh.position ));
+				//if (d > 4.60) {
+				//camera.translateZ(-1 * (d-4.60) );
+				//}
+				
+				var deslocX = (chassisMesh.position.x - camera.position.x) * .2 * dt;
+				var deslocY = (chassisMesh.position.y - camera.position.y) * .2 * dt;
+				var deslocZ = (chassisMesh.position.z - camera.position.z) * .2 * dt;
+
+				camera.position.x += deslocX;
+				camera.position.y  = chassisMesh.position.y;
+				camera.position.z += deslocZ;
+
+				camera.lookAt(chassisMesh.position);
+				var d = Math.abs(camera.position.distanceTo( chassisMesh.position ));
+				if (d > 4.60) {
+					camera.translateZ(-1 * (d-4.60) );
+				}
+				//camera.position.y += margem_up.margem_3pessoa_up;
+				camera.translateY(margem_up.margem_3pessoa_up);
+				camera.lookAt(chassisMesh.position);
+
+				// DEBUG CAM
+				//camera.position.copy(chassisMesh.position);
+				//camera.position.x += 5;
+				//camera.lookAt(chassisMesh.position);
+			}
+
+			/*********/
+			// FBX OBJ
+			if (carregouModelo) {
+				trasesq.position.copy(wheelMeshes[BACK_LEFT].position);
+				trasesq.quaternion.copy(wheelMeshes[BACK_LEFT].quaternion);
+
+				object3DTrasEsq.position.copy(chassisMesh.position);
+				object3DTrasEsq.quaternion.copy(chassisMesh.quaternion);
+				object3DTrasEsq.translateX(-.9);
+				object3DTrasEsq.translateZ(-1.235);
+				object3DTrasEsq.translateY(-.18);
+
+				// trasdir
+				trasdir.position.copy(wheelMeshes[BACK_RIGHT].position);
+				trasdir.quaternion.copy(wheelMeshes[BACK_RIGHT].quaternion);
+
+				object3DTrasDir.position.copy(chassisMesh.position);
+				object3DTrasDir.quaternion.copy(chassisMesh.quaternion);
+				object3DTrasDir.translateX(.9);
+				object3DTrasDir.translateZ(-1.235);
+				object3DTrasDir.translateY(-.18);
+
+				// frenteesq
+				frenteesq.position.copy(wheelMeshes[FRONT_LEFT].position);
+				frenteesq.quaternion.copy(wheelMeshes[FRONT_LEFT].quaternion);
+
+				object3DFrenteEsq.position.copy(chassisMesh.position);
+				object3DFrenteEsq.quaternion.copy(chassisMesh.quaternion);
+				object3DFrenteEsq.translateX(.9);
+				object3DFrenteEsq.translateZ(1.15);
+				object3DFrenteEsq.translateY(-.2);
+
+				// frentedir
+				frentedir.position.copy(wheelMeshes[FRONT_RIGHT].position);
+				frentedir.quaternion.copy(wheelMeshes[FRONT_RIGHT].quaternion);
+
+				object3DFrenteDir.position.copy(chassisMesh.position);
+				object3DFrenteDir.quaternion.copy(chassisMesh.quaternion);
+				object3DFrenteDir.translateX(-.9);
+				object3DFrenteDir.translateZ(1.15);
+				object3DFrenteDir.translateY(-.2);
+
+				// corpo
+				corpo.position.copy(chassisMesh.position);
+				corpo.quaternion.copy(chassisMesh.quaternion);
+
+				frenteesq.position.x = object3DFrenteEsq.position.x;
+				frenteesq.position.y = object3DFrenteEsq.position.y;
+				frenteesq.position.z = object3DFrenteEsq.position.z;
+
+				frentedir.position.x = object3DFrenteDir.position.x;
+				frentedir.position.y = object3DFrenteDir.position.y;
+				frentedir.position.z = object3DFrenteDir.position.z;
+
+				trasdir.position.x = object3DTrasDir.position.x;
+				trasdir.position.y = object3DTrasDir.position.y;
+				trasdir.position.z = object3DTrasDir.position.z;
+
+				trasesq.position.x = object3DTrasEsq.position.x;
+				trasesq.position.y = object3DTrasEsq.position.y;
+				trasesq.position.z = object3DTrasEsq.position.z;
+
+				corpo.visible = true;
+			 	trasesq.visible = true;
+			 	trasdir.visible = true;
+			 	frenteesq.visible = true;
+			 	frentedir.visible = true;
+			}
+		}
+		/**************/
+	 };
+
+	 if (camera) {
+	 	window.addEventListener( 'keydown', keydown );
+	 	window.addEventListener( 'keyup', keyup );
+	 }
+
+	 this.getBody = function () {
+	 	return chassisRigidBody;
+	 }
+
+	 this.getVehicle = function () {
+	 	return vehicle;
+	 }
+
+}
+
+export { Carro };
---
+++ examples/Fisica.js
@@ -0,0 +1,184 @@
+// NECESSARIO IMPORTAR o ammo.js externamente
+import * as THREE from '../build/three.module.js';
+
+function Fisica(objetos, funcaoColisao, gravityConstant, funcaoLoad) {
+	var instancia = this;
+
+	this.objetos = objetos;
+	this.funcaoColisao = funcaoColisao;
+	this.funcaoLoad = funcaoLoad;
+
+	this.rigidBodies = [];
+	this.gravityConstant = gravityConstant;
+	if (this.gravityConstant == undefined) {
+		this.gravityConstant = 7.8;
+	}
+	this.collisionConfiguration;
+	this.dispatcher;
+	this.broadphase;
+	this.solver;
+	this.physicsWorld;
+	this.margin = 0.05;
+	this.transformAux1;
+	this.tempBtVec3_1;
+	this.loaded = false;
+
+	this.createObject = function (obj) {
+		var massa = obj.massa;
+		var friccao = obj.friccao;
+		var convexo = obj.convexo;
+		var margin = obj.margin;
+		if (massa == undefined) {
+			massa = 0;
+		}
+		if (friccao == undefined) {
+			friccao = 0.5;
+		}
+		if (convexo == undefined) {
+			convexo = true;
+		}
+		if (margin == undefined) {
+			margin = this.margin;
+		}
+		var shape;
+		if (convexo) {
+			shape = this.createConvexHullPhysicsShape(obj, new Ammo.btConvexHullShape());
+			shape.setMargin(margin);
+		} else {
+			var mesh = this.createTriangleShapeByGeometry(obj, new Ammo.btTriangleMesh(true, true));
+			shape = new Ammo.btBvhTriangleMeshShape(mesh, true, true);
+			shape.setMargin(margin);
+		}
+		
+		var transform = new Ammo.btTransform();
+		transform.setIdentity();
+		transform.setOrigin( new Ammo.btVector3( obj.position.x, obj.position.y, obj.position.z ) );
+		transform.setRotation( new Ammo.btQuaternion( obj.quaternion.x, obj.quaternion.y, obj.quaternion.z, obj.quaternion.w ) );
+		var motionState = new Ammo.btDefaultMotionState( transform );
+		var localInertia = new Ammo.btVector3( 0, 0, 0 );
+		shape.calculateLocalInertia( massa, localInertia );
+		var rbInfo = new Ammo.btRigidBodyConstructionInfo( massa, motionState, shape, localInertia );
+		var body = new Ammo.btRigidBody( rbInfo );
+		body.setFriction( friccao );
+		obj.userData.physicsBody = body;
+		body.objeto = obj; // Objeto threejs
+		if (massa > 0) {
+			this.rigidBodies.push(obj);
+			// Disable deactivation
+			body.setActivationState( 4 );
+		}
+		this.physicsWorld.addRigidBody(body);
+
+		var btVecUserData = new Ammo.btVector3( 0, 0, 0 );
+		btVecUserData.threeObject = obj;
+		body.setUserPointer( btVecUserData );
+		
+	};
+
+	this.createTriangleShapeByGeometry = function(obj, mesh) {
+
+		if (obj.children && obj.children.length > 0) {
+			for (var i = 0; i < obj.children.length; i++) {
+				this.createTriangleShapeByGeometry(obj.children[i], mesh);
+			}
+			return mesh;
+		}
+
+		var geometry = obj.geometry;
+
+		if (geometry.isBufferGeometry) {
+			geometry = new THREE.Geometry().fromBufferGeometry(geometry);
+		}
+
+		var vertices = geometry.vertices;
+		for (var i = 0; i < geometry.faces.length; i++) {
+			var face = geometry.faces[i];
+			mesh.addTriangle(
+				new Ammo.btVector3(vertices[face.a].x, vertices[face.a].y, vertices[face.a].z),
+				new Ammo.btVector3(vertices[face.b].x, vertices[face.b].y, vertices[face.b].z),
+				new Ammo.btVector3(vertices[face.c].x, vertices[face.c].y, vertices[face.c].z),
+				false
+			);
+		}
+		return mesh;
+	};
+
+	this.createConvexHullPhysicsShape = function(obj, shape) {
+		if (obj.children && obj.children.length > 0) {
+			for (var i = 0; i < obj.children.length; i++) {
+				this.createConvexHullPhysicsShape(obj.children[i], shape);
+			}
+			return shape;
+		}
+		
+		var coords = obj.geometry.attributes.position.array;
+		for ( var i = 0, il = coords.length; i < il; i += 3 ) {
+			this.tempBtVec3_1.setValue( coords[ i ], coords[ i + 1 ], coords[ i + 2 ] );
+			var lastOne = ( i >= ( il - 3 ) );
+			shape.addPoint( this.tempBtVec3_1, lastOne );
+		}
+		return shape;
+	};
+
+	this.render = function(deltaTime) {
+		if (!this.loaded) {
+			return;
+		}
+
+		// Step world
+		this.physicsWorld.stepSimulation( deltaTime, 10 );
+
+		// Update rigid bodies
+		for ( var i = 0, il = this.rigidBodies.length; i < il; i ++ ) {
+			var objThree = this.rigidBodies[ i ];
+			var objPhys = objThree.userData.physicsBody;
+			var ms = objPhys.getMotionState();
+
+			if (ms) {
+				ms.getWorldTransform( this.transformAux1 );
+				var p = this.transformAux1.getOrigin();
+				var q = this.transformAux1.getRotation();
+				objThree.position.set( p.x(), p.y(), p.z() );
+				objThree.quaternion.set( q.x(), q.y(), q.z(), q.w() );
+			}
+		}
+
+		for (var i = 0, il = this.dispatcher.getNumManifolds(); i < il; i ++) {
+			var contactManifold = this.dispatcher.getManifoldByIndexInternal( i );
+			var rb0 = Ammo.castObject( contactManifold.getBody0(), Ammo.btRigidBody );
+			var rb1 = Ammo.castObject( contactManifold.getBody1(), Ammo.btRigidBody );
+			if (this.funcaoColisao != undefined) {
+				this.funcaoColisao(rb0, rb1);
+			}
+		}
+	};
+
+	Ammo().then(function(AmmoLib) {
+		Ammo = AmmoLib;
+
+		// Init
+		instancia.collisionConfiguration = new Ammo.btDefaultCollisionConfiguration();
+		instancia.dispatcher = new Ammo.btCollisionDispatcher( instancia.collisionConfiguration );
+		instancia.broadphase = new Ammo.btDbvtBroadphase();
+		instancia.solver = new Ammo.btSequentialImpulseConstraintSolver();
+		instancia.physicsWorld = new Ammo.btDiscreteDynamicsWorld( instancia.dispatcher, instancia.broadphase, instancia.solver, instancia.collisionConfiguration );
+		instancia.physicsWorld.setGravity( new Ammo.btVector3( 0, - instancia.gravityConstant, 0 ) );
+		instancia.transformAux1 = new Ammo.btTransform();
+		instancia.tempBtVec3_1 = new Ammo.btVector3( 0, 0, 0 );
+		
+
+		for (var i = 0; i < instancia.objetos.length; i++) {
+			instancia.createObject(instancia.objetos[i]);
+		}
+
+		instancia.loaded = true;
+
+		if (instancia.funcaoLoad != undefined) {
+			instancia.funcaoLoad(instancia);
+		}
+
+	});
+
+}
+
+export { Fisica };
